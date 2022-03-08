d982a98eda712cbbed7e2d0ff427fa7165a228b8
KeiSEMXD <keitermadison@gmail.com>
2022-03-08 15:36:50 -0400

    Se agrega el proyecto2

---
+++ Form1.Designer.cs
@@ -0,0 +1,350 @@
+﻿namespace CalculadoraGUI_version_final_
+{
+    partial class FPrincipal
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Bseven = new System.Windows.Forms.Button();
+            this.Beigth = new System.Windows.Forms.Button();
+            this.Bnine = new System.Windows.Forms.Button();
+            this.Bdividir = new System.Windows.Forms.Button();
+            this.Bfour = new System.Windows.Forms.Button();
+            this.Bcero = new System.Windows.Forms.Button();
+            this.Bpunto = new System.Windows.Forms.Button();
+            this.Btwo = new System.Windows.Forms.Button();
+            this.Bfive = new System.Windows.Forms.Button();
+            this.Bc = new System.Windows.Forms.Button();
+            this.Bthree = new System.Windows.Forms.Button();
+            this.Bsix = new System.Windows.Forms.Button();
+            this.Brestar = new System.Windows.Forms.Button();
+            this.Bsumar = new System.Windows.Forms.Button();
+            this.Bmultiplicar = new System.Windows.Forms.Button();
+            this.Bigual = new System.Windows.Forms.Button();
+            this.Bones = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Belevacion = new System.Windows.Forms.Button();
+            this.BRaiz2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Bseven
+            // 
+            this.Bseven.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Bseven.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Bseven.Location = new System.Drawing.Point(14, 145);
+            this.Bseven.Name = "Bseven";
+            this.Bseven.Size = new System.Drawing.Size(75, 36);
+            this.Bseven.TabIndex = 0;
+            this.Bseven.Text = "7";
+            this.Bseven.UseVisualStyleBackColor = false;
+            this.Bseven.Click += new System.EventHandler(this.Bseven_Click);
+            // 
+            // Beigth
+            // 
+            this.Beigth.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Beigth.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Beigth.Location = new System.Drawing.Point(112, 145);
+            this.Beigth.Name = "Beigth";
+            this.Beigth.Size = new System.Drawing.Size(75, 36);
+            this.Beigth.TabIndex = 1;
+            this.Beigth.Text = "8";
+            this.Beigth.UseVisualStyleBackColor = false;
+            this.Beigth.Click += new System.EventHandler(this.Beigth_Click);
+            // 
+            // Bnine
+            // 
+            this.Bnine.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Bnine.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Bnine.Location = new System.Drawing.Point(208, 145);
+            this.Bnine.Name = "Bnine";
+            this.Bnine.Size = new System.Drawing.Size(75, 36);
+            this.Bnine.TabIndex = 2;
+            this.Bnine.Text = "9";
+            this.Bnine.UseVisualStyleBackColor = false;
+            this.Bnine.Click += new System.EventHandler(this.Bnine_Click);
+            // 
+            // Bdividir
+            // 
+            this.Bdividir.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Bdividir.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Bdividir.Location = new System.Drawing.Point(14, 103);
+            this.Bdividir.Name = "Bdividir";
+            this.Bdividir.Size = new System.Drawing.Size(75, 36);
+            this.Bdividir.TabIndex = 3;
+            this.Bdividir.Text = "/";
+            this.Bdividir.UseVisualStyleBackColor = false;
+            this.Bdividir.Click += new System.EventHandler(this.Bdividir_Click);
+            // 
+            // Bfour
+            // 
+            this.Bfour.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Bfour.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Bfour.Location = new System.Drawing.Point(14, 187);
+            this.Bfour.Name = "Bfour";
+            this.Bfour.Size = new System.Drawing.Size(75, 36);
+            this.Bfour.TabIndex = 5;
+            this.Bfour.Text = "4";
+            this.Bfour.UseVisualStyleBackColor = false;
+            this.Bfour.Click += new System.EventHandler(this.Bfour_Click);
+            // 
+            // Bcero
+            // 
+            this.Bcero.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Bcero.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Bcero.Location = new System.Drawing.Point(14, 268);
+            this.Bcero.Name = "Bcero";
+            this.Bcero.Size = new System.Drawing.Size(75, 36);
+            this.Bcero.TabIndex = 7;
+            this.Bcero.Text = "0";
+            this.Bcero.UseVisualStyleBackColor = false;
+            this.Bcero.Click += new System.EventHandler(this.Bcero_Click);
+            // 
+            // Bpunto
+            // 
+            this.Bpunto.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Bpunto.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Bpunto.Location = new System.Drawing.Point(112, 268);
+            this.Bpunto.Name = "Bpunto";
+            this.Bpunto.Size = new System.Drawing.Size(75, 36);
+            this.Bpunto.TabIndex = 10;
+            this.Bpunto.Text = ".";
+            this.Bpunto.UseVisualStyleBackColor = false;
+            this.Bpunto.Click += new System.EventHandler(this.Bpunto_Click);
+            // 
+            // Btwo
+            // 
+            this.Btwo.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Btwo.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Btwo.Location = new System.Drawing.Point(112, 226);
+            this.Btwo.Name = "Btwo";
+            this.Btwo.Size = new System.Drawing.Size(75, 36);
+            this.Btwo.TabIndex = 9;
+            this.Btwo.Text = "2";
+            this.Btwo.UseVisualStyleBackColor = false;
+            this.Btwo.Click += new System.EventHandler(this.Btwo_Click);
+            // 
+            // Bfive
+            // 
+            this.Bfive.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Bfive.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Bfive.Location = new System.Drawing.Point(112, 187);
+            this.Bfive.Name = "Bfive";
+            this.Bfive.Size = new System.Drawing.Size(75, 36);
+            this.Bfive.TabIndex = 8;
+            this.Bfive.Text = "5";
+            this.Bfive.UseVisualStyleBackColor = false;
+            this.Bfive.Click += new System.EventHandler(this.Bfive_Click);
+            // 
+            // Bc
+            // 
+            this.Bc.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Bc.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Bc.Location = new System.Drawing.Point(208, 268);
+            this.Bc.Name = "Bc";
+            this.Bc.Size = new System.Drawing.Size(75, 36);
+            this.Bc.TabIndex = 13;
+            this.Bc.Text = "C";
+            this.Bc.UseVisualStyleBackColor = false;
+            this.Bc.Click += new System.EventHandler(this.Bc_Click);
+            // 
+            // Bthree
+            // 
+            this.Bthree.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Bthree.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Bthree.Location = new System.Drawing.Point(208, 226);
+            this.Bthree.Name = "Bthree";
+            this.Bthree.Size = new System.Drawing.Size(75, 36);
+            this.Bthree.TabIndex = 12;
+            this.Bthree.Text = "3";
+            this.Bthree.UseVisualStyleBackColor = false;
+            this.Bthree.Click += new System.EventHandler(this.Bthree_Click);
+            // 
+            // Bsix
+            // 
+            this.Bsix.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Bsix.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Bsix.Location = new System.Drawing.Point(208, 187);
+            this.Bsix.Name = "Bsix";
+            this.Bsix.Size = new System.Drawing.Size(75, 36);
+            this.Bsix.TabIndex = 11;
+            this.Bsix.Text = "6";
+            this.Bsix.UseVisualStyleBackColor = false;
+            this.Bsix.Click += new System.EventHandler(this.Bsix_Click);
+            // 
+            // Brestar
+            // 
+            this.Brestar.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Brestar.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Brestar.Location = new System.Drawing.Point(287, 103);
+            this.Brestar.Name = "Brestar";
+            this.Brestar.Size = new System.Drawing.Size(75, 36);
+            this.Brestar.TabIndex = 16;
+            this.Brestar.Text = "-";
+            this.Brestar.UseVisualStyleBackColor = false;
+            this.Brestar.Click += new System.EventHandler(this.Brestar_Click);
+            // 
+            // Bsumar
+            // 
+            this.Bsumar.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Bsumar.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Bsumar.Location = new System.Drawing.Point(112, 103);
+            this.Bsumar.Name = "Bsumar";
+            this.Bsumar.Size = new System.Drawing.Size(75, 36);
+            this.Bsumar.TabIndex = 15;
+            this.Bsumar.Text = "+";
+            this.Bsumar.UseVisualStyleBackColor = false;
+            this.Bsumar.Click += new System.EventHandler(this.Bsumar_Click);
+            // 
+            // Bmultiplicar
+            // 
+            this.Bmultiplicar.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Bmultiplicar.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Bmultiplicar.Location = new System.Drawing.Point(208, 103);
+            this.Bmultiplicar.Name = "Bmultiplicar";
+            this.Bmultiplicar.Size = new System.Drawing.Size(75, 36);
+            this.Bmultiplicar.TabIndex = 14;
+            this.Bmultiplicar.Text = "*";
+            this.Bmultiplicar.UseVisualStyleBackColor = false;
+            this.Bmultiplicar.Click += new System.EventHandler(this.Bmultiplicar_Click);
+            // 
+            // Bigual
+            // 
+            this.Bigual.BackColor = System.Drawing.Color.BlueViolet;
+            this.Bigual.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Bigual.Location = new System.Drawing.Point(287, 229);
+            this.Bigual.Name = "Bigual";
+            this.Bigual.Size = new System.Drawing.Size(75, 77);
+            this.Bigual.TabIndex = 17;
+            this.Bigual.Text = "=";
+            this.Bigual.UseVisualStyleBackColor = false;
+            this.Bigual.Click += new System.EventHandler(this.Bigual_Click);
+            // 
+            // Bones
+            // 
+            this.Bones.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Bones.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Bones.Location = new System.Drawing.Point(14, 226);
+            this.Bones.Name = "Bones";
+            this.Bones.Size = new System.Drawing.Size(75, 36);
+            this.Bones.TabIndex = 18;
+            this.Bones.Text = "1";
+            this.Bones.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.Bones.UseVisualStyleBackColor = false;
+            this.Bones.Click += new System.EventHandler(this.Bones_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Times New Roman", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.Location = new System.Drawing.Point(14, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(348, 81);
+            this.textBox1.TabIndex = 19;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Belevacion
+            // 
+            this.Belevacion.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Belevacion.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Belevacion.Location = new System.Drawing.Point(287, 187);
+            this.Belevacion.Name = "Belevacion";
+            this.Belevacion.Size = new System.Drawing.Size(75, 36);
+            this.Belevacion.TabIndex = 21;
+            this.Belevacion.Text = "x²";
+            this.Belevacion.UseVisualStyleBackColor = false;
+            this.Belevacion.Click += new System.EventHandler(this.Belevacion_Click);
+            // 
+            // BRaiz2
+            // 
+            this.BRaiz2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BRaiz2.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.BRaiz2.Location = new System.Drawing.Point(287, 145);
+            this.BRaiz2.Name = "BRaiz2";
+            this.BRaiz2.Size = new System.Drawing.Size(75, 36);
+            this.BRaiz2.TabIndex = 20;
+            this.BRaiz2.Text = "√";
+            this.BRaiz2.UseVisualStyleBackColor = false;
+            this.BRaiz2.Click += new System.EventHandler(this.BRaiz2_Click);
+            // 
+            // FPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(375, 317);
+            this.Controls.Add(this.Belevacion);
+            this.Controls.Add(this.BRaiz2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Bones);
+            this.Controls.Add(this.Bigual);
+            this.Controls.Add(this.Brestar);
+            this.Controls.Add(this.Bsumar);
+            this.Controls.Add(this.Bmultiplicar);
+            this.Controls.Add(this.Bc);
+            this.Controls.Add(this.Bthree);
+            this.Controls.Add(this.Bsix);
+            this.Controls.Add(this.Bpunto);
+            this.Controls.Add(this.Btwo);
+            this.Controls.Add(this.Bfive);
+            this.Controls.Add(this.Bcero);
+            this.Controls.Add(this.Bfour);
+            this.Controls.Add(this.Bdividir);
+            this.Controls.Add(this.Bnine);
+            this.Controls.Add(this.Beigth);
+            this.Controls.Add(this.Bseven);
+            this.Name = "FPrincipal";
+            this.ShowIcon = false;
+            this.Text = "Calculadora";
+            this.Load += new System.EventHandler(this.FPrincipal_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button Bseven;
+        private Button Beigth;
+        private Button Bnine;
+        private Button Bdividir;
+      
+        private Button Bfour;
+        
+        private Button Bcero;
+        private Button Bpunto;
+        private Button Btwo;
+        private Button Bfive;
+        private Button Bc;
+        private Button Bthree;
+        private Button Bsix;
+        private Button Brestar;
+        private Button Bsumar;
+        private Button Bmultiplicar;
+        private Button Bigual;
+        private Button Bones;
+        private TextBox textBox1;
+        private Button Belevacion;
+        private Button BRaiz2;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,222 @@
+﻿namespace CalculadoraGUI_version_final_
+{
+    public partial class FPrincipal : Form
+    {
+        string input = string.Empty;
+        string operando1 = string.Empty;
+        string operando2 = string.Empty;
+        char operacion;
+        double resultado = 0.0;
+
+        public FPrincipal()
+        {
+            InitializeComponent();
+        }
+
+      
+
+
+
+        private void Bcero_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text = "";
+            input += "0";
+            this.textBox1.Text += input;
+        }
+
+        private void Bpunto_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text = "";
+            input += ".";
+            this.textBox1.Text += input; 
+        }
+
+        private void Bc_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text = "";
+            input += "C";
+            this.input = string.Empty;
+            this.operando1 = string.Empty;
+            this.operando2= string.Empty;
+            
+           
+        }
+
+        private void Brestar_Click(object sender, EventArgs e)
+        {
+            operando1 = input;
+            operacion = '-';
+            input = string.Empty;
+        }
+
+        private void Bones_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text = "";
+            input += "1";
+            this.textBox1.Text += input;
+        }
+
+        private void Btwo_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text = "";
+            input += "2";
+            this.textBox1.Text += input;
+        }
+
+        private void Bthree_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text = "";
+            input += "3";
+            this.textBox1.Text += input;
+        }
+
+        private void Bfour_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text = "";
+            input += "4";
+            this.textBox1.Text += input;
+        }
+
+        private void Bfive_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text = "";
+            input += "5";
+            this.textBox1.Text += input;
+        }
+
+        private void Bsix_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text = "";
+            input += "6";
+            this.textBox1.Text += input;
+        }
+
+        private void Bseven_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text = "";
+            input += "7";
+            this.textBox1.Text += input;
+        }
+
+        private void Beigth_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text = "";
+            input += "8";
+            this.textBox1.Text += input;
+        }
+
+        private void Bnine_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text = "";
+            input += "9";
+            this.textBox1.Text += input;
+        }
+
+        private void Bsumar_Click(object sender, EventArgs e)
+        {
+            operando1 = input;
+            operacion = '+';
+            input = string.Empty;
+        }
+
+        private void Bmultiplicar_Click(object sender, EventArgs e)
+        {
+            operando1 = input;
+            operacion = '*';
+            input = string.Empty;
+        }
+
+        private void Bdividir_Click(object sender, EventArgs e)
+        {
+            operando1 = input;
+            operacion = '/';
+            input = string.Empty;
+        }
+
+        private void Bigual_Click(object sender, EventArgs e)
+        {
+            operando2 = input;
+            double num1, num2;
+            double.TryParse(operando1, out num1);
+            double.TryParse(operando2, out num2);
+
+            this.textBox1.Text = "";
+            this.input = string.Empty;
+            this.operando1 = string.Empty;
+            this.operando2 = string.Empty;
+
+
+            if(operacion == '+')
+            {
+              resultado = num1 + num2;
+              textBox1.Text = resultado.ToString();
+            }
+             else if(operacion == '-') {
+                resultado = num1 - num2;
+                textBox1.Text = resultado.ToString();
+
+              }
+            else if (operacion == '*')
+            {
+                resultado = num1 * num2;
+                textBox1.Text = resultado.ToString();
+
+            }
+            else if (operacion == '/')
+            {
+                if(operacion != '0')
+                {
+                    resultado = num1 / num2;
+                    textBox1.Text = resultado.ToString();
+
+                }
+                else
+                {
+                    textBox1.Text = "No se puede dividir entre 0";
+                }
+               
+
+            }
+
+            else if(operacion == '√')
+            {
+                resultado = Math.Sqrt(num1);
+                textBox1.Text = resultado.ToString();
+
+            }
+
+            else if (operacion == 'x')
+            {
+                int exponente = 2;
+                resultado = Math.Pow(num1, exponente);
+                textBox1.Text = resultado.ToString();
+            }
+            
+        }
+
+        private void FPrincipal_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BRaiz2_Click(object sender, EventArgs e)
+        {
+            operando1 = input;
+            operacion = '√';
+            input = string.Empty;
+
+        }
+
+        private void Belevacion_Click(object sender, EventArgs e)
+        {
+            operando1 = input;
+            operacion = 'x';
+            input = string.Empty;
+        }
+    }
+}
